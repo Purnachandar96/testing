5c237a3d912dc626643a904630b11681650b7e94
Ubuntu <ubuntu@ip-172-31-1-227.ap-south-1.compute.internal>
2019-11-27 08:10:53 +0000

    new changes

--- src/components/test.js
+++ src/components/test.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TestComponent() {
     return (
         <>
-        <h6 style={{fontColor:'#fff'}}>fsafsafsdfasfa</h6> 
+        <h6 style={{fontColor:'#fff'}}>Welcome to Jenkins</h6> 
         </>
     )
 }
