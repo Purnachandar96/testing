67b7aac2d7fb875b04411396cd55757b48847e1b
Ubuntu <ubuntu@ip-172-31-1-227.ap-south-1.compute.internal>
2019-11-27 07:50:53 +0000

    new commit1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       
+      	 Edit <code>src/App.js</code> and save to reload.
+
           Hey!! Lets rock Jenkins and work slaves.
         </p>
         <a
